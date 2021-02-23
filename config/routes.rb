5f57792f951c9b9aad96e837c7f13beccd9028cd
Antony Parker <antonyqparker@gmail.com>
2021-02-23 11:54:20 +0100

    Changing string to text in model

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-#  resources :pets, only: [:new, :create, :show, :index], do
-#    resources :bookings, only: [:create]
-#  end
-#  resources bookings:, only: [:index, :edit, :update]
+  resources :pets, only: [:new, :create, :show, :index] do
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:index, :edit, :update]
 end
